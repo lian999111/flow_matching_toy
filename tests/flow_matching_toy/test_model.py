33ae47d96188035243bafdf2a2d7000ec8effd8e
Jui-Lin Liang <lian999111@gmail.com>
2025-09-29 15:48:45 +0200

    Add basic model

---
+++ tests/flow_matching_toy/test_model.py
@@ -0,0 +1,15 @@
+import torch
+
+from flow_matching_toy.model import MLP
+
+
+class TestMLP:
+    def test_output_shape(self):
+        model = MLP(in_channels=2, channels=16, time_emb_channels=8, layers=1)
+        batch_size = 4
+        x = torch.rand((batch_size, 2))
+        t = torch.rand((batch_size,))
+
+        y = model(x, t)
+
+        assert y.shape == x.shape
